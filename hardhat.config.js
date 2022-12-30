8c7fd9afb17132cd5d0d27838873855c6e1b53fa
Swarnabha Sinha <swarnabhasinha.201mn059@nitk.edu.in>
2022-12-31 03:51:56 +0530

    add:Project setup & Nft contract

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+    solidity: "0.8.4",
+    networks: {
+        hardhat: {
+            chainId: 31337
+        },
+    },
+    // defaultNetwork: "ropsten",
+    // networks: {
+    //   hardhat: {
+    //   },
+    //   ropsten: {
+    //     url: ["ALCHEMY URL"],
+    //     accounts: ['METAMASK/OTHER WALLET PRIVATE KEY'],
+    //   }
+    // },
+};
---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        NFT_MARKET_CONTRACT_ADDRESS:"0x770fA3FBE1af3B4675611C1dAC2CeB6C64A877fb",
+        NFT_CONTRACT_ADDRESS:"0x21f9669CE0705Dfd91ac653749ef0ED4d9E3C912",
+        CHAIN_ID:3
+    },
+    reactStrictMode: true,
+    images: {
+        domains: ['ipfs.infura.io'],
+    },
+}
+
+module.exports = nextConfig
